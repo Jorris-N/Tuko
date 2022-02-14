419fea928cfc6d663270cbf9851ae6bf7c6d3f42
Jorris-N <nyangejorris@gmail.com>
2022-02-14 14:11:36 +0300

    new page added and styled ; cart page

---
+++ products.js
@@ -0,0 +1,73 @@
+var products = {
+    119: {
+        name : "Roasted Chicken",
+        desc : "",
+        img : "special1.jpg",
+        price : 1200
+    },
+    120: {
+        name : "Pizza",
+        desc : "",
+        img : "special2.jpg",
+        price : 850
+    },
+    121: {
+        name : "Lamb Chops",
+        desc : "",
+        img : "special3.jpg",
+        price : 600
+        },
+    122: {
+         name : "Rice Stew",
+        desc : "",
+        img : "louis-hansel-mVZ_gjm_TOk-unsplash.jpg",
+        price : 450
+    },
+    123: {
+      name : "Coffee Breakfast",
+      desc : "",
+      img : "brkfst.jpg",
+      price : 700
+    },
+    127: {
+      name : "Egg Sandwich",
+      desc : "",
+      img : "omlet.jpg",
+      price : 350
+    },
+    125: {
+      name : "Macaroni",
+      desc : "",
+      img : "macoroni.jpg",
+      price : 400
+    },
+    126: {
+      name : "fruit Salad",
+      desc : "",
+      img : "monika-grabkowska-114R2tFk8gI-unsplash.jpg",
+      price : 400
+    },
+    124: {
+        name : "Whole Grain weetabix",
+        img : "hanny-naibaho-6JUjdlHyqw0-unsplash.jpg",
+        price : 300
+    },
+    128: {
+        name : "Brocolli Salad",
+        desc : "",
+        img : "ella-olsson-ZjEeMnDiq00-unsplash.jpg",
+        price : 450
+    },
+    129: {
+        name : "Humberger",
+        desc : "",
+        img : "vicky-ng-KsmkVW0Hsv0-unsplash.jpg",
+        price : 950
+    },
+    130 : {
+        name : "Pasta",
+        desc : "",
+        img : "rui-silvestre-D3lKRy7A_FY-unsplash.jpg",
+        price : 300
+    }
+  };
